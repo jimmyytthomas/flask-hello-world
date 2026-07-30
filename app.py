0b4c5236c4f1b7d90b9a254dd92cc1a39819cba6
Jimmy Thomas <jamesgthomas7@gmail.com>
2026-07-30 19:24:16 +0000

    Add database connection test

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+import os 
+import psycopg2
+
 app = Flask(__name__)
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Jimmy Thomas in 3308!'
+
+
+@app.route('/db_test')
+def db_test():
+    try:
+        conn = get_db_connection()
+        conn.close()
+        return "Database connection successful!"
+    except Exception as e:
+        return f"Database connection failed: {e}"
